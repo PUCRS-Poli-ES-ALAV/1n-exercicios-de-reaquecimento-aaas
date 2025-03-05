b46463c91a15fe21ce45ad794d8b601dce046e4d
André Machado <124793837+ALVMAC@users.noreply.github.com>
2025-03-05 18:51:14 +0000

    3

--- exs.c
+++ exs.c
@@ -13,10 +13,16 @@ int somas(int a, int b){
     else return 1 + 1 + somas(a-1,b-1);
 }
 
+double somasSobreN(int a, float b){
+    if (a < b) return;
+    return 1/b + somasSobreN(a, b+1);
+}
+
 int main(){
     int res = multSoma(3,4, 0);
     printf("mult = %d\n", res);
     res = somas(1,4);
     printf("somas = %d\n", res);
+    printf("Soma /n: %.2f\n", somasSobreN(2,1));
 
 }
